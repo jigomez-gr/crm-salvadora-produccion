a800d45a28d3b8c464d832f37924c9965e36e28a
jigomez <jigomez@hotmail.com>
2026-09-05 20:54:15 +0200

    fix(sms): import AuthModule into ZadarmaSmsModule to resolve JwtAuthGuard dependencies

--- backend/src/sms/zadarma-sms.module.ts
+++ backend/src/sms/zadarma-sms.module.ts
@@ -4,9 +4,13 @@ import { ZadarmaSmsLog } from './zadarma-sms-log.entity';
 import { VapiAccount } from '../common/entities/vapi-account.entity';
 import { ZadarmaSmsService } from './zadarma-sms.service';
 import { ZadarmaSmsController } from './zadarma-sms.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ZadarmaSmsLog, VapiAccount])],
+  imports: [
+    TypeOrmModule.forFeature([ZadarmaSmsLog, VapiAccount]),
+    AuthModule,
+  ],
   controllers: [ZadarmaSmsController],
   providers: [ZadarmaSmsService],
   exports: [ZadarmaSmsService],
